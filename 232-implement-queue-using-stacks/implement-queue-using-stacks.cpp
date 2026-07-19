3f408dcc48c692e019a0dd4c164120ebe4234828
Pawan Kushwaha <kuswahapawan60@gmail.com>
2026-07-20 00:05:15 +0530

    Time: 2 ms (3.56%) | Memory: 10.1 MB (74.64%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,3 +1,4 @@
+//method 2
 class MyQueue {
 public:
 stack<int>st;
@@ -7,38 +8,35 @@ stack<int>helper;
     }
     
     void push(int x) {
-        st.push(x);
+        if(st.size()==0){
+             st.push(x);
+            return;
+        }
+        else{
+            while(st.size()>0){
+                helper.push(st.top());
+                st.pop();
+            }
+            st.push(x);
+            while(helper.size()>0){
+             st.push(helper.top());
+             helper.pop();
+            }
+
+        }
+       
         
     }
     
     int pop() {//remove at bottom...use heloer stack
-        while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x=helper.top();
-        helper.pop();
-
-         while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return x;
+       int x=st.top();
+       st.pop();
+       return x;
 
     }
     
     int peek() {//return front
-         while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x=helper.top();
-         while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return x;
-
+      return st.top(); 
     }
     
     bool empty() {
