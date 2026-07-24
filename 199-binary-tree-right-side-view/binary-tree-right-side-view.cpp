727baaa8c86533c1a30d28819b65b96ffb0677c7
Pawan Kushwaha <kuswahapawan60@gmail.com>
2026-07-24 11:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (85.81%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
 
-    int levels(TreeNode* root) {
-        if (root == NULL) return 0;
+    int levels(TreeNode* root){
+        if(root==NULL) return 0;
         return 1 + max(levels(root->left), levels(root->right));
     }
 
-    void nthLevel(TreeNode* root, int curr, int level, vector<int>& ans) {
+    void nthLevel(TreeNode* root, int curr, int level, vector<int>& ans){
+        if(root==NULL) return;
 
-        if (root == NULL) return;
-
-        if (curr == level) {
-            // Same level ka last visited node store hoga
-            ans[level] = root->val;
+        if(curr==level){
+            ans[level] = root->val;   // Fixed (curr -> level)
             return;
         }
 
         // Left pehle, right baad me
-        // Right overwrite karega => Right View
-        nthLevel(root->left, curr + 1, level, ans);
-        nthLevel(root->right, curr + 1, level, ans);
+        // Right node overwrite karega => Right Side View
+        nthLevel(root->left, curr+1, level, ans);
+        nthLevel(root->right, curr+1, level, ans);
     }
 
-    void levelOrder(TreeNode* root, vector<int>& ans) {
+    void levelOrder(TreeNode* root, vector<int>& ans){
+        int n = ans.size();
 
-        int h = levels(root);
-
-        for (int i = 0; i < h; i++) {
+        for(int i=0; i<n; i++){
             nthLevel(root, 0, i, ans);
         }
     }
 
     vector<int> rightSideView(TreeNode* root) {
 
-        if (root == NULL) return {};
+        if(root==NULL) return {};
 
-        vector<int> ans(levels(root));
+        vector<int> ans(levels(root),0);
 
         levelOrder(root, ans);
 
