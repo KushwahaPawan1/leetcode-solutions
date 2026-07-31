6da05948e338b369dafc014820e0584babeac3e0
Pawan Kushwaha <kuswahapawan60@gmail.com>
2026-07-31 21:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (91.39%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,65 +1,29 @@
 class Solution {
 public:
+    void insert(TreeNode* root, int val) {
 
-    // Preorder aur Inorder ki help se BST construct karega.
-    TreeNode* build(vector<int>& pre, int prelo, int prehi,
-                    vector<int>& in, int inlo, int inhi) {
+        if(root->val > val) { // go left
 
-        // Agar range invalid ho gayi to node exist nahi karega.
-        if(prelo > prehi) return NULL;
+            if(root->left == NULL)
+                root->left = new TreeNode(val);
 
-        // Preorder ka first element hamesha root hota hai.
-        TreeNode* root = new TreeNode(pre[prelo]);
-
-        // Sirf ek hi node bachi hai.
-        if(prelo == prehi) return root;
-
-        // Inorder me root ki position dhoondo.
-        int i = inlo;
-        while(i <= inhi) {
-            if(in[i] == pre[prelo]) break;
-            i++;
+            else
+                insert(root->left, val);
         }
+        else { // go right
 
-        // Left subtree me kitne nodes hain.
-        int leftCount = i - inlo;
-
-        // Right subtree me kitne nodes hain.
-        int rightCount = inhi - i;
-
-        // Left subtree construct karo.
-        root->left = build(pre,
-                           prelo + 1,
-                           prelo + leftCount,
-                           in,
-                           inlo,
-                           i - 1);
-
-        // Right subtree construct karo.
-        root->right = build(pre,
-                            prelo + leftCount + 1,
-                            prehi,
-                            in,
-                            i + 1,
-                            inhi);
-
-        // Current subtree ka root return karo.
-        return root;
+            if(root->right == NULL)
+                root->right = new TreeNode(val);
+            else
+                insert(root->right, val);
+        }
     }
 
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-
-        // Preorder ki copy banao.
-        vector<int> in = preorder;
-
-        // Copy ko sort kar do.
-        // Sorted array hi inorder traversal hota hai BST ka.
-        sort(in.begin(), in.end());
-
-        int n = preorder.size();
-
-        // Preorder + Inorder se BST construct karo.
-        return build(preorder, 0, n - 1,
-                     in, 0, n - 1);
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+        TreeNode* root = new TreeNode(pre[0]);
+        for(int i = 1; i < pre.size(); i++) {
+            insert(root, pre[i]);
+        }
+        return root;
     }
 };
