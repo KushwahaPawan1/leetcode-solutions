bcac441b7810137d97383862f8ae215421fdfbc9
Pawan Kushwaha <kuswahapawan60@gmail.com>
2026-08-04 22:52:52 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (12.24%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -1,16 +1,49 @@
 class Solution {
 public:
     int maximumNumberOfStringPairs(vector<string>& arr) {
+
+        // Total number of strings
         int n = arr.size();
-        int count =0;
-        //unordered set
-        for(int i=0;i<n-1;i++){
-            string rev=arr[i];
-            reverse(rev.begin(),rev.end());
-            for(int j=i+1;j<n;j++){
-                if(rev==arr[j]) count++;
+
+        // Final answer (number of valid pairs)
+        int count = 0;
+
+        // Unordered set me saari strings store karenge
+        // Taaki O(1) average time me search kar sake
+        unordered_set<string> s;
+
+        // Saari strings set me insert kar do
+        for (int i = 0; i < n; i++) {
+            s.insert(arr[i]);
+        }
+
+        // Har string ke liye check karenge ki uska reverse present hai ya nahi
+        for (int i = 0; i < n; i++) {
+
+            // Current string ki copy banao
+            string rev = arr[i];
+
+            // Us copy ko reverse kar do
+            reverse(rev.begin(), rev.end());
+
+            // Agar string already palindrome hai (jaise "aa")
+            // to usse pair nahi bana sakte, isliye skip kar do
+            if (arr[i] == rev)
+                continue;
+
+            // Agar reverse string set me present hai
+            if (s.find(rev) != s.end()) {
+
+                // Ek valid pair mil gaya
+                count++;
+
+                // Current string ko set se remove kar do
+                // Taaki same pair dobara count na ho
+                s.erase(arr[i]);
             }
         }
+
+        // Total valid pairs return karo
         return count;
     }
 };
