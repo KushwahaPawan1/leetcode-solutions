f5016b6a90d73e2763d5d16231b3b579917871ab
Pawan Kushwaha <kuswahapawan60@gmail.com>
2026-08-06 16:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (3.33%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-
+int prodOfDigit(int num){
+    string number = to_string(num);
+    int prod=1;
+    for(int i=0;i<number.length();i++)  prod = prod * (number[i] - '0');
+      return prod;
+}
     int smallestNumber(int n, int t) {
-
-        // n ko string me convert kiya
-        string number = to_string(n);   // Example: n=15 -> "15"
-
-        int prod = 1;
-
-        // n ke saare digits ka product nikalo
-        for (int i = 0; i < number.length(); i++) {
-            prod = prod * (number[i] - '0');
-        }
-
-        // Agar n ke digits ka product t se divisible hai
-        // to n hi smallest number hai
-        if (prod % t == 0)
-            return n;
-
+       int currProd= prodOfDigit(n);
+        if(currProd%t==0) return n;
         else {
-
-            // n+1, n+2, n+3... check karte jao
-            for (int i = 1; ; i++) {
-
-                // Current number ko string me convert karo
-                number = to_string(n + i);
-
-                int prod = 1;
-
-                // Current number ke digits ka product nikalo
-                for (int j = 0; j < number.length(); j++) {
-                    prod = prod * (number[j] - '0');
-                }
-
-                // Agar product t se divisible hai
-                // to current number hi answer hai
-                if (prod % t == 0)
-                    return n + i;
+            while(currProd%t!=0){
+                n++;
+                currProd= prodOfDigit(n);
             }
         }
+         return n;
     }
 };
